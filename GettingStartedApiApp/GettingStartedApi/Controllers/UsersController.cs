443a06c3282d61005aaec7b13a218f8384c5aaf7
egleticia <leticiaegle@yahoo.com.br>
2023-05-15 20:54:00 -0300

    Http Verbs

--- GettingStartedApiApp/GettingStartedApi/Controllers/UsersController.cs
+++ GettingStartedApiApp/GettingStartedApi/Controllers/UsersController.cs
@@ -6,33 +6,42 @@ namespace GettingStartedApi.Controllers;
 [ApiController]
 public class UsersController : ControllerBase
 {
-    // GET: api/<UsersController>
+    // GET: api/Users
     [HttpGet]
     public IEnumerable<string> Get()
     {
         return new string[] { "value1", "value2" };
     }
 
-    // GET api/<UsersController>/5
+    // GET api/Users/5
     [HttpGet("{id}")]
     public string Get(int id)
     {
         return "value";
     }
-
-    // POST api/<UsersController>
+    // Creates a new record
+    // POST api/Users
     [HttpPost]
     public void Post([FromBody] string value)
     {
     }
-
-    // PUT api/<UsersController>/5
+    // Updates a whole record (or possibly creates)
+    // PUT api/Users/5
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] string value)
     {
     }
 
-    // DELETE api/<UsersController>/5
+    // Updates part of a record
+    //PATCH api/Users/5
+    [HttpPatch("{id}")]
+    public void Patch(int id, [FromBody] string emailAddress)
+    {
+
+    }
+
+    // Deletes a record
+    // DELETE api/Users/5
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
