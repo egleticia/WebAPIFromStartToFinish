db17f11ea145eaedf154efe73ba4ad6ffd62171b
egleticia <leticiaegle@yahoo.com.br>
2023-05-30 22:38:20 -0300

    Implementing API Versioning - Part II

---
+++ VersionedApiApp/VersionedApi/Controllers/V1/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionedApi.Controllers.V1;
+
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+public class UsersController : ControllerBase
+{
+    // GET: api/UsersController
+    //GET: api/v1/users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "VERSION 1 value1", "VERSION 1 value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
