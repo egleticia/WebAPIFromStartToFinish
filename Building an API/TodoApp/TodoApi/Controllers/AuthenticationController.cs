d27a8ff436e368e653b8d3cfd77846b2e9d30327
egleticia <leticiaegle@yahoo.com.br>
2023-06-16 22:16:16 -0300

    Adding Basic Security

---
+++ Building an API/TodoApp/TodoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _config;
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int Id, string FirstName, string LastName, string UserName);
+
+    [HttpPost("token")]
+    [AllowAnonymous]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        var user = ValidateCredentials(data);
+
+        if (user is null)
+            return Unauthorized();
+
+        string token = GenerateToken(user);
+
+        return Ok(token);
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(
+            Encoding.ASCII.GetBytes(
+                _config.GetValue<string>("Authentication:SecretKey")));
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+        claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
+        claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
+
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddMinutes(1),
+            signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        //THIS IS NOT PRODUCTION CODE - REPLACE THIS WITH A CALL TO YOUR AUTH SYSTEM
+
+        if (CompareValues(data.UserName, "egleticia") && CompareValues(data.Password, "Test123"))
+            return new UserData(1, "Leticia", "Egle", data.UserName!);
+
+        if (CompareValues(data.UserName, "tcorey") && CompareValues(data.Password, "Test123"))
+            return new UserData(2, "Tim", "Corey", data.UserName!);
+        
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string? expected)
+    {
+        if (actual is not null)
+        {
+            if (actual == expected)
+                return true;
+        }
+        return false;
+    }
+}
