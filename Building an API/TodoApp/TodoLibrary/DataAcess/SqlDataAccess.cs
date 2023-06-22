3d0349d3175af6b20339555e5d3efd656078debc
egleticia <leticiaegle@yahoo.com.br>
2023-06-21 22:12:49 -0300

    Add Data Access - Part I

--- Building an API/TodoApp/TodoLibrary/DataAcess/SqlDataAccess.cs
+++ Building an API/TodoApp/TodoLibrary/DataAcess/SqlDataAccess.cs
@@ -1,7 +1,40 @@
-﻿namespace TodoLibrary.DataAcess
+﻿using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace TodoLibrary.DataAcess;
+
+public class SqlDataAccess : ISqlDataAccess
 {
-    public class SqlDataAccess
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
     {
-        //iniciando Library 
+        _config = config;
     }
+
+    public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        //using -  it will close the connection no matter what in the end of '}'
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        var rows = await connection.QueryAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
+
+    public Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        return connection.ExecuteAsync(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+    }
+
 }
---
+++ Building an API/TodoApp/TodoLibrary/DataAcess/TodoData.cs
@@ -0,0 +1,22 @@
+﻿using TodoLibrary.Models;
+
+namespace TodoLibrary.DataAcess
+{
+    public class TodoData
+    {
+        private readonly ISqlDataAccess _sql;
+        public TodoData(ISqlDataAccess sql)
+        {
+            _sql= sql;
+        }
+
+        public Task<List<TodoModel>> GetAllAssigned(int assignedTo)
+        {
+           return _sql.LoadData<TodoModel, dynamic>(
+                "dbo.spTodos_GetAllAssigned",
+                new { AssignedTo = assignedTo },
+                "");
+        }
+
+    }
+}
