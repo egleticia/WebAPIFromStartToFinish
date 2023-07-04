9b8c047715c6e41a7662f4b9326a56a83314a82c
egleticia <leticiaegle@yahoo.com.br>
2023-07-03 21:39:43 -0300

    Add Data Access - Part II

--- Building an API/TodoApp/TodoLibrary/DataAcess/TodoData.cs
+++ Building an API/TodoApp/TodoLibrary/DataAcess/TodoData.cs
@@ -15,7 +15,17 @@ namespace TodoLibrary.DataAcess
            return _sql.LoadData<TodoModel, dynamic>(
                 "dbo.spTodos_GetAllAssigned",
                 new { AssignedTo = assignedTo },
-                "");
+                "Default");
+        }
+
+        public async Task<TodoModel?>GetOneAssigned(int assignedTo, int todoId)
+        {
+            var result = await _sql.LoadData<TodoModel, dynamic>(
+                 "dbo.spTodos_GetAllAssigned",
+                 new { AssignedTo = assignedTo, TodoId = todoId },
+                 "Default");
+
+            return result.FirstOrDefault();
         }
 
     }
